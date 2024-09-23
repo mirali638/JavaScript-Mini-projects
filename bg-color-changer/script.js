0f553e6dfc749b20f167d7cbf56156848bab9197
Mirali Jivani <146528110+mirali638@users.noreply.github.com>
2024-09-23 16:54:45 +0530

    Update script.js

--- bg-color-changer/script.js
+++ bg-color-changer/script.js
@@ -1,1 +1,35 @@
 
+const box = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+box.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "red") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "pink") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "purple") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "green") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "orange") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
