3041e99f5e5c224517fac0272a73cb50614decc3
Mirali Jivani <146528110+mirali638@users.noreply.github.com>
2025-02-14 17:33:58 +0530

    Update app.js

--- Simon Says Game/app.js
+++ Simon Says Game/app.js
@@ -1,1 +1,79 @@
 
+let gameSeq = [];
+let userSeq = [];
+
+let started = false;
+let level = 0;
+
+let btns = ["one", "two", "three", "four"];
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+  if (started == false) {
+    started = true; // Corrected here
+    levelUp();
+  }
+});
+
+function btnFlash(btn) {
+  btn.classList.add("flash");
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 250);
+}
+
+function userFlash(btn) {
+  btn.classList.add("userflash");
+  setTimeout(function () {
+    btn.classList.remove("userflash");
+  }, 250);
+}
+
+function levelUp() {
+  userSeq = [];
+  level++;
+  h2.innerText = `Level ${level}`;
+
+  let randomIdx = Math.floor(Math.random() * 4); 
+  let randomColor = btns[randomIdx];
+  let randBtn = document.querySelector(`.${randomColor}`);
+  gameSeq.push(randomColor);
+  btnFlash(randBtn);
+}
+
+function checkAns(idx) {
+  if (userSeq[idx] === gameSeq[idx]) {
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to restart`; 
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(function () {
+      document.querySelector("body").style.backgroundColor = "white";
+    }, 150);
+    reset();
+  }
+}
+
+function btnPress() {
+  let btn = this;
+  userFlash(btn);
+
+  let userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
+  checkAns(userSeq.length - 1);
+}
+
+let allbtns = document.querySelectorAll(".btn");
+for (let btn of allbtns) { 
+  btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+  started = false;
+  userSeq = [];
+  gameSeq = [];
+  level = 0;
+}
